15cc4c956de335699d48185bde55a984fe5b9f47
Eric Binnion <ericbinnion@gmail.com>
2015-06-23 13:16:11 -0500

    WordPress.com logging in test now functional

--- index.js
+++ index.js
@@ -1,15 +1,48 @@
-var webdriverio = require( 'webdriverio' );
-var options = {
-	desiredCapabilities: {
-		browserName: 'chrome'
-	}
-};
+var webdriverio = require( 'webdriverio' ),
+	assert = require( 'assert' );
 
-webdriverio
-	.remote(options)
-	.init()
-	.url('http://www.google.com')
-	.title(function(err, res) {
-		console.log('Title was: ' + res.value);
-	})
-	.end();
+var config = require( './config' );
+
+describe( 'Logging into WordPress.com', function() {
+	var client = {};
+	this.timeout( 99999999 );
+
+	before( function( done ) {
+		client = webdriverio.remote( {
+			desiredCapabilities: { browserName: 'chrome' }
+		} );
+
+		client.init( done );
+	} );
+
+	it( 'login button exists when user not logged in', function( done ) {
+		client
+			.url( 'https://wordpress.com' )
+			.click( '.click-wpcom-login', function( err ) {
+				assert( undefined === err, 'there was not an error clicking the login button' );
+			} )
+			.call( done );
+	} );
+
+	it( 'login form exists and user can log in', function( done ) {
+		client
+			.waitFor( '#loginform', 2000, function( err ) {
+				assert( undefined === err, 'loginform is visible' );
+			} )
+			.setValue( '#user_login', config.username, function( err ) {
+				assert( undefined === err, 'there was not an error filling in the username' );
+			} )
+			.setValue( '#user_pass', config.password, function( err ) {
+				assert( undefined === err, 'there was not an error filling in the password' );
+			} )
+			.submitForm( '#loginform' )
+			.waitFor( 'ul.menu-right li.me a', 2000, function( err ) {
+				assert( undefined === err, 'login was successful' );
+			} )
+			.call( done );
+	} );
+
+	after( function( done ) {
+		client.end( done );
+	} );
+} );
